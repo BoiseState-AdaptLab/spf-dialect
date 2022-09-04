b253750d985b1b906e8e4a1e485c88e026499a03
AaronStGeorge <aaronstgeorge@gmail.com>
2022-09-04 12:16:33 -0600

    Add operation

--- standalone-opt/standalone-opt.cpp
+++ standalone-opt/standalone-opt.cpp
@@ -24,16 +24,18 @@
 
 int main(int argc, char **argv)
 {
-  mlir::registerAllPasses();
+//  mlir::registerAllPasses();
   // TODO: Register standalone passes here.
 
   mlir::DialectRegistry registry;
-  // registry.insert<mlir::standalone::StandaloneDialect,
-  //                 mlir::arith::ArithmeticDialect, mlir::func::FuncDialect>();
+   registry.insert<mlir::standalone::StandaloneDialect,
+                   mlir::arith::ArithmeticDialect,
+                   mlir::func::FuncDialect,
+                   mlir::vector::VectorDialect>();
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
-  registerAllDialects(registry);
+//  registerAllDialects(registry);
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "Standalone optimizer driver\n", registry));
