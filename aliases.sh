f78f74107993ff9fe33b1d324f642493c0f4da74
AaronStGeorge <aaronstgeorge@gmail.com>
2022-11-16 20:22:47 -0700

    Fix asan issue. Turns out strstream is not what you want.

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
 
 export LLVM_SYMBOLIZER_PATH="../llvm-project/build/bin/llvm-symbolizer"
 alias b="make -C build standalone-opt"
-alias r="build/bin/standalone-opt --my-pass --debug-only=my-pass test.mlir"
+alias rd="ASAN_OPTIONS=detect_leaks=0 build/bin/standalone-opt --my-pass --debug-only=my-pass dense_mttkrp_test.mlir"
+alias rs="ASAN_OPTIONS=detect_leaks=0 build/bin/standalone-opt --my-pass --debug-only=my-pass sparse_mttkrp_test.mlir"
