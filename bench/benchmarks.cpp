87e1e602dd1af5a625210066a3f5670b797bf96f
AaronStGeorge <aaronstgeorge@gmail.com>
2023-01-16 09:05:54 -0700

    IEGenLib TTM working, needs some cleanup.

--- bench/benchmarks.cpp
+++ bench/benchmarks.cpp
@@ -156,7 +156,28 @@ bool areCoordsEqualExceptMode(COO &coo, uint64_t exceptMode, uint64_t i,
   return true;
 }
 
-std::vector<uint64_t> fiberidx(COO &sortedCoo, uint64_t constantMode) {
+// fiberStartStopIndices returns the indices at which fibers in COO tensor
+// <sortedCoo> formed by holding <constantMode> constant begin and end.
+// <sortedCoo> is assumed to have been sorted lexigraphically with
+// <constantMode> considered that last mode.
+//
+// Ex: the following COO mode 3 tensor is sorted lexigraphically with mode 0
+//    (constant mode) last
+// mode: 0,1,2
+// 0|    1 0 0 : 77
+// 1|    0 0 2 : 3
+// 2|    1 0 2 : 10
+// 3|    0 0 3 : 63
+// ⬑ index
+// output will be: [0,1,3,4].
+//  - Fiber 0 starts at index 0 (always)
+//  - Fiber 0 ends (and fiber 1 starts) at index 1 (as at index 1, one two one
+//    of the non-constant dimensions from index 0 differ at mode 2: 0 to 2)
+//  - Fiber 1 (and fiber 2 starts) ends at index 3 (as at index 3 one of the two
+//    non-constant dimensions that are the same for index 1 and 2 differ at mode
+//    2: 2 to 3)
+//  - As fiber 2 is the last fiber it ends at the last index +1;
+std::vector<uint64_t> fiberStartStopIndices(COO &sortedCoo, uint64_t constantMode) {
   std::vector<uint64_t> out;
   uint64_t lastIdx = sortedCoo.nnz;
   for (uint64_t i = 0; i < sortedCoo.nnz; i++) {
@@ -173,16 +194,18 @@ std::vector<uint64_t> fiberidx(COO &sortedCoo, uint64_t constantMode) {
 int64_t cpu_ttm_iegenlib(bool debug, int64_t iterations, char *filename) {
   COO *Xdata = (COO *)_mlir_ciface_read_coo(filename);
 
+  uint64_t constantMode = 0;
+
   Xdata->dump(std::cout);
   Xdata->sortIndicesModeLast(0);
   std::cout << "============================================\n";
   Xdata->dump(std::cout);
 
-  std::vector<uint64_t> fb = fiberidx(*Xdata, 0);
+  std::vector<uint64_t> fiberStartStop = fiberStartStopIndices(*Xdata, constantMode);
 
   bool first = true;
   std::cout << "fg: [";
-  for (auto i : fb) {
+  for (auto i : fiberStartStop) {
     if (first) {
       first = false;
     } else {
@@ -192,8 +215,47 @@ int64_t cpu_ttm_iegenlib(bool debug, int64_t iterations, char *filename) {
   }
   std::cout << "]\n";
 
-  uint64_t NNZ = 5;
-  uint64_t NCOLS = 5;
+  uint64_t NFIBERS = fiberStartStop.size() -1;
+
+  uint64_t NCOLS = 2;
+
+    // Construct U matrix
+    StridedMemRefType<double, 2> u;
+    auto un = Xdata->dims[constantMode];
+    auto um = NCOLS;
+    auto uData = std::vector<double>(un * um);
+    for (uint64_t i = 0; i < un; i++) {
+      for (uint64_t j = 0; j < um; j++) {
+        uData[i * um + j] = i * um + j;
+      }
+    }
+    u.basePtr = u.data = uData.data();
+    u.offset = 0;
+    u.sizes[0] = un;
+    u.sizes[1] = um;
+    u.strides[0] = um;
+    u.strides[1] = 1;
+
+    // Construct Y matrix
+    StridedMemRefType<double, 2> y;
+    auto yn = NFIBERS;
+    auto ym = NCOLS;
+    auto yData = std::vector<double>(NFIBERS * NCOLS);
+    std::fill(yData.begin(), yData.end(), 0.0);
+    y.basePtr = y.data = yData.data();
+    y.offset = 0;
+    y.sizes[0] = yn;
+    y.sizes[1] = ym;
+    y.strides[0] = ym;
+    y.strides[1] = 1;
+
+#define Y(i, k) y.data[i * NCOLS + k]
+#define X(z) Xdata->values[z]
+#define U(r, k) u.data[r * NCOLS + k]
+
+  uint64_t *UFib = fiberStartStop.data();
+
+  uint64_t *UFr = Xdata->coord[constantMode].data();
 
   uint64_t t1, t2, t3, t4, t5, t6;
 
@@ -201,46 +263,57 @@ int64_t cpu_ttm_iegenlib(bool debug, int64_t iterations, char *filename) {
 
 #undef s0
 #undef s_0
-#define s_0(z, ib, ie, j, r, k) Y(i, k) += X(j) * U(r, k)
-#define s0(z, ib, ie, j, r, k) s_0(z, ib, ie, j, r, k);
+#define s_0(z, ib, ie, j, r, k)   Y(z,k) += X(j) * U(r,k)
+#define s0(z, ib, ie, j, r, k)   s_0(z, ib, ie, j, r, k);
 
 #undef UFib_0
-#undef UFie_1
+#undef UFib_1
 #undef UFr_2
 #define UFib(t0) UFib[t0]
 #define UFib_0(__tv0) UFib(__tv0)
-#define UFie(t0) UFie[t0]
-#define UFie_1(__tv0) UFie(__tv0)
+#define UFib_1(__tv0) UFib(__tv0 + 1)
 #define UFr(t0) UFr[t0]
 #define UFr_2(__tv0, __tv1, __tv2, __tv3) UFr(__tv3)
 
-  t1 = 0;
-  t2 = 0;
-  t3 = 0;
-  t4 = 0;
-  t5 = 0;
-  t6 = 0;
+t1 = 0;
+t2 = 0;
+t3 = 0;
+t4 = 0;
+t5 = 0;
+t6 = 0;
 
-  if (NCOLS >= 1) {
-    for (t1 = 0; t1 <= NNZ - 1; t1++) {
-      t2 = UFib_0(t1);
-      t3 = UFie_1(t1);
-      for (t4 = UFib_0(t1); t4 <= UFie_1(t1); t4++) {
-        t5 = UFr_2(t1, t2, t3, t4);
-        for (t6 = 0; t6 <= NCOLS - 1; t6++) {
-          s0(t1, t2, t3, t4, t5, t6);
-        }
+if (NCOLS >= 1) {
+  for(t1 = 0; t1 <= NFIBERS-1; t1++) {
+    t2=UFib_0(t1);
+    t3=UFib_1(t1);
+    for(t4 = UFib_0(t1); t4 <= UFib_1(t1)-1; t4++) {
+      t5=UFr_2(t1,t2,t3,t4);
+      for(t6 = 0; t6 <= NCOLS-1; t6++) {
+        s0(t1,t2,t3,t4,t5,t6);
       }
     }
   }
+}
 
 #undef s0
 #undef s_0
 #undef UFib_0
-#undef UFie_1
+#undef UFib_1
 #undef UFr_2
 
   // =============================================
+
+  first = true;
+  std::cout << "y: [";
+  for (auto i : y) {
+    if (first) {
+      first = false;
+    } else {
+      std::cout << ", ";
+    }
+    std::cout << i;
+  }
+  std::cout << "]\n";
   return 0;
 }
 
