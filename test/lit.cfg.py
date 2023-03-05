fa15211548476eecca35a1faf4b9d7ff95c2a3e5
AaronStGeorge <aaronstgeorge@gmail.com>
2023-03-05 14:04:51 -0700

    Removed CAPI and python (stuff that was just sitting around from standalone)

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -50,7 +50,6 @@ llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
 
 tool_dirs = [config.standalone_tools_dir, config.llvm_tools_dir]
 tools = [
-    'standalone-capi-test',
     'spf-opt',
 ]
 
